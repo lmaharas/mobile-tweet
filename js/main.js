5ff9c6f627a6c48bf7591ba573e0f37e4c486d48
lmaharas <lynnmaharas@hotmail.com>
2013-07-03 09:23:17 -0400

    begin repo

---
+++ js/main.js
@@ -0,0 +1,43 @@
+(function ($, undefined) {
+	"use strict";
+	
+	var windowWidth = $(window).width();
+	
+	function hideHomeWrapper() {
+		
+		$('.home-wrapper').animate({
+			left: - windowWidth + 'px'
+		}, 300, showTwitterAcct() );
+		
+	}
+	
+	function showTwitterAcct() {
+		 
+		 $('.tweet-wrapper').animate({
+				left: windowWidth/2 - $('.tweet-wrapper').width()/2 + 'px',
+				display: "inline-block"
+			}, 300); 
+
+	}
+	
+	
+	function bindEvents() {
+		$('.acct a').on('click', function(e){
+			e.preventDefault();
+			hideHomeWrapper();			
+		});
+		
+		$(window).resize( function () {
+			windowWidth = $(window).width();
+		});
+	}
+	
+	function loadBody() {
+		bindEvents();
+	}
+	
+	$(document).ready(function() {
+		loadBody();
+	});
+	
+})(jQuery);
